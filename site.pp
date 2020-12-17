558dfa48f71ed613dd4095ec30bcbe46b9dcaed7
Javier Jáuregui <javierjaureguij1996@gmail.com>
2020-12-16 18:02:49 -0600

    Se actuailizó site.pp

--- site.pp
+++ site.pp
@@ -1,5 +1,11 @@
 include mymodule
-exec {'test111_docker':
+exec {'test_docker':
   command => "/bin/bash -c 'docker rm $(docker ps -aq) -f' || true",
-  onlyif => "/bin/bash -c 'test -e /home/jenkins/deployments.txt'",
+  unless => "/bin/bash -c 'test -e /home/jenkins/deployments.txt'",
+}
+
+exec {'remove_flag_file':
+	command => "/bin/bash -c 'rm /home/jenkins/deployments.txt'",
+	onlyif => "/bin/bash -c 'test -e /home/jenkins/deployments.txt'",
+	require => Exec['test_docker'],
 }
