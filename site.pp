8aa2ae70b38bd68c747a9797284eb530dbdd9d20
Javier Jáuregui <javierjaureguij1996@gmail.com>
2020-12-16 18:11:48 -0600

    Se actuailizó site.pp

--- site.pp
+++ site.pp
@@ -1,6 +1,5 @@
 include mymodule
 exec {'test_docker':
   command => "/bin/bash -c 'docker rm $(docker ps -aq) -f' || true",
-  unless => "/bin/bash -c 'test -e /home/jenkins/deployments.txt'",
   refreshonly => true,
 }
